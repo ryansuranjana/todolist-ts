f66ef94e430be00fa44d91e91cdb016087d35781
ryansuranjana <ryansuranjana23@gmail.com>
2023-09-19 22:39:59 +0800

    create store

---
+++ src/store/index.ts
@@ -0,0 +1,45 @@
+interface Todos {
+    id: number;
+    todo: string;
+}
+
+const state = (): Todos[] => {
+    const storedData = localStorage.getItem("todos");
+
+    if (storedData) {
+        const todos: Todos[] = JSON.parse(storedData);
+        return todos;
+    } else {
+        return [];
+    }
+};
+
+const save = (todo: string): void => {
+    const todos: Todos[] = state();
+    const id: number = Date.now() + Math.random();
+
+    todos.unshift({ id: id, todo: todo });
+    localStorage.setItem("todos", JSON.stringify(todos));
+};
+
+const update = (id: number, todo: string): void => {
+    const todos: Todos[] = state();
+    const findIndex: number = todos.findIndex((todo) => todo.id === id);
+
+    todos[findIndex] = { id, todo };
+    localStorage.setItem("todos", JSON.stringify(todos));
+};
+
+const find = (id: number): Todos | null => {
+    const todos: Todos[] = state();
+    return todos.find((todo) => todo.id === id) || null;
+};
+
+const destroy = (id: number): void => {
+    const todos: Todos[] = state();
+    const todosUpdate: Todos[] = todos.filter((todo) => todo.id !== id);
+
+    localStorage.setItem("todos", JSON.stringify(todosUpdate));
+};
+
+export default { state, save, update, find, destroy };
